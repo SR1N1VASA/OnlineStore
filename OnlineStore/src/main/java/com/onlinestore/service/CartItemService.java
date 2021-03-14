3114589e02ac311ac43ac53f18925dff2ab85145
srinivas ayachitula <srinivasayachitula@gmail.com>
2021-03-14 17:21:29 -0400

    Logic for adding book to shopping cart

--- OnlineStore/src/main/java/com/onlinestore/service/CartItemService.java
+++ OnlineStore/src/main/java/com/onlinestore/service/CartItemService.java
@@ -2,11 +2,15 @@ package com.onlinestore.service;
 
 import java.util.List;
 
+import com.onlinestore.domain.Book;
 import com.onlinestore.domain.CartItem;
 import com.onlinestore.domain.ShoppingCart;
+import com.onlinestore.domain.User;
 
 public interface CartItemService {
 	List<CartItem> findByShoppingCart(ShoppingCart shoppingCart);
 	
 	CartItem updateCartItem(CartItem cartItem);
+	
+	CartItem addBookToCartItem(Book book, User user, int qty);
 }
