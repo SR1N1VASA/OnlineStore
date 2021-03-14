14427ebd192271e6233af82d9c2f8594dbcb89f2
srinivas ayachitula <srinivasayachitula@gmail.com>
2021-03-14 18:19:29 -0400

    Deleting and Updating the Items in Shopping Cart

--- OnlineStore/src/main/java/com/onlinestore/repository/BookToCartItemRepository.java
+++ OnlineStore/src/main/java/com/onlinestore/repository/BookToCartItemRepository.java
@@ -1,9 +1,14 @@
 package com.onlinestore.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.onlinestore.domain.BookToCartItem;
+import com.onlinestore.domain.CartItem;
 
+
+@Transactional
 public interface BookToCartItemRepository extends CrudRepository<BookToCartItem, Long> {
+	void deleteByCartItem(CartItem cartItem);
 
 }
