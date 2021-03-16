c45e7f9b78b5f92348213424850d73fca8ddf11b
srinivas ayachitula <srinivasayachitula@gmail.com>
2021-03-15 21:37:32 -0400

    Category Based Search

---
+++ OnlineStore/src/main/java/com/onlinestore/controller/SearchController.java
@@ -0,0 +1,52 @@
+package com.onlinestore.controller;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.onlinestore.domain.Book;
+import com.onlinestore.domain.User;
+import com.onlinestore.service.BookService;
+import com.onlinestore.service.UserService;
+
+@Controller
+public class SearchController {
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private BookService bookService;
+
+	@RequestMapping("/searchByCategory")
+	public String searchByCategory(
+			@RequestParam("category") String category,
+			Model model, Principal principal
+			){
+		if(principal!=null) {
+			String username = principal.getName();
+			User user = userService.findByUsername(username);
+			model.addAttribute("user", user);
+		}
+		
+		String classActiveCategory = "active"+category;
+		classActiveCategory = classActiveCategory.replaceAll("\\s+", "");
+		classActiveCategory = classActiveCategory.replaceAll("&", "");
+		model.addAttribute(classActiveCategory, true);
+		
+		List<Book> bookList = bookService.findByCategory(category);
+		
+		if (bookList.isEmpty()) {
+			model.addAttribute("emptyList", true);
+			return "bookshelf";
+		}
+		
+		model.addAttribute("bookList", bookList);
+		
+		return "bookshelf";
+	}
+}
